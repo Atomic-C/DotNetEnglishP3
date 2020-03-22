88269834ddf3db7e34555847c2a7d03b5dd4701d
pedro sousa <pedro.p.sousa@sapo.pt>
2020-03-22 19:10:51 -0100

    Added client aise annotation for Name Stock and Price on ProductViewModel;

--- P3AddNewFunctionalityDotNetCore/Models/ViewModels/ProductViewModel.cs
+++ P3AddNewFunctionalityDotNetCore/Models/ViewModels/ProductViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
 
 namespace P3AddNewFunctionalityDotNetCore.Models.ViewModels
 {
@@ -6,15 +7,15 @@ namespace P3AddNewFunctionalityDotNetCore.Models.ViewModels
     {
         [BindNever]
         public int Id { get; set; }
-
+        [Required]
         public string Name { get; set; }
 
         public string Description { get; set; }
 
         public string Details { get; set; }
-
+        [Required]
         public string Stock { get; set; }
-
+        [Required]
         public string Price { get; set; }
     }
 }
